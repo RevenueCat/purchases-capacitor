8bfc8df144778f1e7958ce0d1c45a4b16371eb36
martindonadieu <martindonadieu@gmail.com>
2022-01-21 14:28:59 +0000

    fix: android issue

--- android/src/main/java/ee/forgr/plugin/capacitor_purchases/CapacitorPurchasesPlugin.java
+++ android/src/main/java/ee/forgr/plugin/capacitor_purchases/CapacitorPurchasesPlugin.java
@@ -2,36 +2,26 @@ package ee.forgr.plugin.capacitor_purchases;
 
 import androidx.annotation.NonNull;
 
-import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.getcapacitor.PluginResult;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
 import com.revenuecat.purchases.PurchaserInfo;
 import com.revenuecat.purchases.Purchases;
 
-import androidx.annotation.Nullable;
-
 import com.revenuecat.purchases.hybridcommon.CommonKt;
 import com.revenuecat.purchases.hybridcommon.ErrorContainer;
 import com.revenuecat.purchases.hybridcommon.OnResult;
-import com.revenuecat.purchases.hybridcommon.OnResultList;
-import com.revenuecat.purchases.hybridcommon.OnResultAny;
 import com.revenuecat.purchases.common.PlatformInfo;
-import com.revenuecat.purchases.hybridcommon.SubscriberAttributesKt;
 import com.revenuecat.purchases.hybridcommon.mappers.PurchaserInfoMapperKt;
 import com.revenuecat.purchases.interfaces.UpdatedPurchaserInfoListener;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -42,7 +32,7 @@ public class CapacitorPurchasesPlugin extends Plugin {
     public static final String PLUGIN_VERSION = "2.4.0";
 
     @PluginMethod
-    private void setup(PluginCall call) {
+    public void setup(PluginCall call) {
         String apiKey = call.getString("apiKey");
         String appUserID = call.getString("appUserID");
         PlatformInfo platformInfo = new PlatformInfo(PLATFORM_NAME, PLUGIN_VERSION);
@@ -58,68 +48,13 @@ public class CapacitorPurchasesPlugin extends Plugin {
         });
     }
 
-    // @PluginMethod
-    // public void addAttributionData(PluginCall call) {
-    //     String networkUserId = call.getString("networkUserId");
-    //     Integer network = call.getInt("networkUserId");
-    //     JSONObject data = call.getObject("data");
-
-    //     SubscriberAttributesKt.addAttributionData(data, network, networkUserId);
-    //     call.resolve();
-    // }
-
     @PluginMethod
-    private void getOfferings(PluginCall call) {
+    public void getOfferings(PluginCall call) {
         CommonKt.getOfferings(getOnResult(call));
     }
 
-//    @PluginMethod
-//    private void getProductInfo(PluginCall call) {
-//        JSONArray productIDs = call.getArray("productIDs");
-//        String type = call.getString("type");
-//        List<String> productIDList = new ArrayList<>();
-//        for (int i = 0; i < productIDs.length(); i++) {
-//            try {
-//                productIDList.add(productIDs.getString(i));
-//            } catch (JSONException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        CommonKt.getProductInfo(productIDList, type, new OnResultList() {
-//            @Override
-//            public void onReceived(List<Map<String, ?>> map) {
-//                JSONArray writableArray = new JSONArray();
-//                JSObject ret = new JSObject();
-//                for (Map<String, ?> detail : map) {
-//                    writableArray.put(convertMapToJson(detail));
-//                }
-//                ret.put("products", writableArray);
-//                call.resolve(ret);
-//            }
-//
-//            @Override
-//            public void onError(ErrorContainer errorContainer) {
-//                call.resolve(convertMapToJson(errorContainer.getInfo()));
-//
-//            }
-//        });
-//    }
-
-    // @PluginMethod
-    // private void purchaseProduct(PluginCall call) {
-    //     String identifier = call.getString("identifier");
-    //     String type = call.getString("type");
-    //     CommonKt.purchaseProduct(
-    //             this.bridge.getActivity(),
-    //             identifier,
-    //             null,
-    //             null,
-    //             type,
-    //             getOnResult(call));
-    // }
-
     @PluginMethod
-    private void purchasePackage(PluginCall call) {
+    public void purchasePackage(PluginCall call) {
         String identifier = call.getString("identifier");
         String offeringIdentifier = call.getString("offeringIdentifier");
         if(identifier == "" || offeringIdentifier == "") {
@@ -135,229 +70,33 @@ public class CapacitorPurchasesPlugin extends Plugin {
                 getOnResult(call));
     }
 
-    // @PluginMethod
-    // public void syncPurchases(PluginCall call) {
-    //     CommonKt.syncPurchases();
-    //     call.resolve();
-    // }
-
-    // @PluginMethod
-    // private void getAppUserID(PluginCall call) {
-    //     JSObject ret = new JSObject();
-    //     ret.put("userId", CommonKt.getAppUserID());
-    //     call.resolve(ret);
-    // }
-
     @PluginMethod
-    private void restoreTransactions(PluginCall call) {
+    public void restoreTransactions(PluginCall call) {
         CommonKt.restoreTransactions(getOnResult(call));
     }
 
     @PluginMethod
-    private void logIn(PluginCall call) {
+    public void logIn(PluginCall call) {
         String appUserID = call.getString("appUserID");
         CommonKt.logIn(appUserID, getOnResult(call));
     }
 
     @PluginMethod
-    private void logOut(PluginCall call) {
+    public void logOut(PluginCall call) {
         CommonKt.logOut(getOnResult(call));
     }
 
     @PluginMethod
-    private void getPurchaserInfo(PluginCall call) {
+    public void getPurchaserInfo(PluginCall call) {
         CommonKt.getPurchaserInfo(getOnResult(call));
     }
 
     @PluginMethod
-    private void setDebugLogsEnabled(PluginCall call) {
+    public void setDebugLogsEnabled(PluginCall call) {
         boolean enabled = call.getBoolean("enabled");
         CommonKt.setDebugLogsEnabled(enabled);
         call.resolve();
     }
-
-//     @PluginMethod
-//     private void isAnonymous(PluginCall call) {
-//         JSObject ret = new JSObject();
-//         ret.put("isAnonymous", CommonKt.isAnonymous());
-//         call.resolve(ret);
-//     }
-
-//     @PluginMethod
-//     private void invalidatePurchaserInfoCache(PluginCall call) {
-//         CommonKt.invalidatePurchaserInfoCache();
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     public void setProxyURLString(PluginCall call) {
-//         String proxyURLString = call.getString("proxyURLString");
-//         CommonKt.setProxyURLString(proxyURLString);
-//         call.resolve();
-//     }
-
-//    @PluginMethod
-//     private void canMakePayments(PluginCall call) {
-//         JSONArray features = call.getArray("features");
-//         ArrayList<Integer> featureList = new ArrayList<>();
-//         try {
-//             if (features != null) {
-//                 for (int i = 0; i < features.length(); i++) {
-//                     featureList.add(features.getInt(i));
-//                 }
-//             }
-//         } catch (JSONException e) {
-//             e.printStackTrace();
-//         }
-
-//         CommonKt.canMakePayments(this.bridge.getActivity(), featureList, new OnResultAny<Boolean>() {
-//             @Override
-//             public void onError(@Nullable ErrorContainer errorContainer) {
-//                 call.resolve(convertMapToJson(errorContainer.getInfo()));
-//             }
-
-//             @Override
-//             public void onReceived(Boolean result) {
-//                 JSObject ret = new JSObject();
-//                 ret.put("canMakePayments", result);
-//                 call.resolve(ret);
-//             }
-//         });
-//     }
-
-//     //================================================================================
-//     // Subscriber Attributes
-//     //================================================================================
-
-//     @PluginMethod
-//     private void setAttributes(PluginCall call) throws JSONException {
-//         JSONObject attributes = call.getObject("attributes");
-//         SubscriberAttributesKt.setAttributes(convertJsonToMap(attributes));
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setEmail(PluginCall call) {
-//         String email = call.getString("email");
-//         SubscriberAttributesKt.setEmail(email);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setPhoneNumber(PluginCall call) {
-//         String phoneNumber = call.getString("phoneNumber");
-//         SubscriberAttributesKt.setPhoneNumber(phoneNumber);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setDisplayName(PluginCall call) {
-//         String displayName = call.getString("displayName");
-//         SubscriberAttributesKt.setDisplayName(displayName);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setPushToken(PluginCall call) {
-//         String pushToken = call.getString("pushToken");
-//         SubscriberAttributesKt.setPushToken(pushToken);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setAdjustID(PluginCall call) {
-//         String adjustID = call.getString("adjustID");
-//         SubscriberAttributesKt.setAdjustID(adjustID);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setAppsflyerID(PluginCall call) {
-//         String appsflyerID = call.getString("appsflyerID");
-//         SubscriberAttributesKt.setAppsflyerID(appsflyerID);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setFBAnonymousID(PluginCall call) {
-//         String fBAnonymousID = call.getString("fBAnonymousID");
-//         SubscriberAttributesKt.setFBAnonymousID(fBAnonymousID);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setMparticleID(PluginCall call) {
-//         String mparticleID = call.getString("mparticleID");
-//         SubscriberAttributesKt.setMparticleID(mparticleID);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setOnesignalID(PluginCall call) {
-//         String onesignalID = call.getString("onesignalID");
-//         SubscriberAttributesKt.setOnesignalID(onesignalID);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setAirshipChannelID(PluginCall call) {
-//         String airshipChannelID = call.getString("airshipChannelID");
-//         SubscriberAttributesKt.setAirshipChannelID(airshipChannelID);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setMediaSource(PluginCall call) {
-//         String mediaSource = call.getString("mediaSource");
-//         SubscriberAttributesKt.setMediaSource(mediaSource);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setCampaign(PluginCall call) {
-//         String campaign = call.getString("campaign");
-//         SubscriberAttributesKt.setCampaign(campaign);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setAdGroup(PluginCall call) {
-//         String adGroup = call.getString("adGroup");
-//         SubscriberAttributesKt.setAdGroup(adGroup);
-//         call.resolve();
-//     }
-
-//    @PluginMethod
-//     private void setAd(PluginCall call) {
-//         String ad = call.getString("ad");
-//         SubscriberAttributesKt.setAd(ad);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setKeyword(PluginCall call) {
-//         String keyword = call.getString("keyword");
-//         SubscriberAttributesKt.setKeyword(keyword);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void setCreative(PluginCall call) {
-//         String creative = call.getString("creative");
-//         SubscriberAttributesKt.setCreative(creative);
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void collectDeviceIdentifiers(PluginCall call) {
-//         SubscriberAttributesKt.collectDeviceIdentifiers();
-//         call.resolve();
-//     }
-
-//     @PluginMethod
-//     private void presentCodeRedemptionSheet(PluginCall call) {
-//         call.resolve();
-//     }
 
     //================================================================================
     // Private methods
@@ -376,20 +115,6 @@ public class CapacitorPurchasesPlugin extends Plugin {
             }
         };
     }
-
-//    private static Map<String, String> convertJsonToMap(JSONObject jsonObject) {
-//        HashMap map = new HashMap();
-//        Iterator keys = jsonObject.keys();
-//        while (keys.hasNext()) {
-//            String key = (String) keys.next();
-//            try {
-//                map.put(key, jsonObject.get(key));
-//            } catch (JSONException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        return map;
-//    }
 
     private static JSObject convertMapToJson(Map<String, ?> readableMap) {
         JSObject object = new JSObject();
