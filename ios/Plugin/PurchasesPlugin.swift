53dbfc96c89ac70569860a77e581fe19bd55b691
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-05-17 17:07:01 +0200

    Version bump for 8.0.0-beta.3

--- ios/Plugin/PurchasesPlugin.swift
+++ ios/Plugin/PurchasesPlugin.swift
@@ -12,7 +12,7 @@ import RevenueCat
 @objc(PurchasesPlugin)
 public class PurchasesPlugin: CAPPlugin, PurchasesDelegate {
     private let platformFlavor = "capacitor"
-    private let platformVersion = "7.5.10"
+    private let platformVersion = "8.0.0-beta.3"
 
     private let customerInfoKey = "customerInfo"
 
