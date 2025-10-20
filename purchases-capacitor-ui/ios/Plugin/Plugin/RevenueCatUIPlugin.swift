RevenueCat/purchases-capacitor

commit c67616837d172820c196970c63773fcd24c64241

Add `presentedOfferingContext` data when presenting paywall (#524)
We were not passing the `presentedOfferingContext` when presenting a
paywall and the user gave us the offering. This fixes that.

purchases-capacitor-ui/ios/Plugin/Plugin/RevenueCatUIPlugin.swift
@@ -44,7 +44,7 @@ public class RevenueCatUIPlugin: CAPPlugin {
                 return
             }
 
-            let offeringIdentifier = call.getObject("offering")?["identifier"]
+            let offeringOptions = self.processOfferingOptions(call)
             let displayCloseButton = call.getBool("displayCloseButton") ?? false
 
             var options: [String: Any] = [
@@ -52,8 +52,10 @@ public class RevenueCatUIPlugin: CAPPlugin {
                 "shouldBlockTouchEvents": true
             ]
 
-            if let offeringIdentifier = offeringIdentifier {
-                options["offeringIdentifier"] = offeringIdentifier
+            if let offeringOptions = offeringOptions {
+                options.merge(offeringOptions) { original, offeringOption in
+                    offeringOption
+                }
             }
 
             proxy.presentPaywall(
@@ -87,7 +89,7 @@ public class RevenueCatUIPlugin: CAPPlugin {
                 return
             }
 
-            let offeringIdentifier = call.getString("offeringIdentifier")
+            let offeringOptions = self.processOfferingOptions(call)
             let displayCloseButton = call.getBool("displayCloseButton") ?? false
 
             var options: [String: Any] = [
@@ -96,8 +98,10 @@ public class RevenueCatUIPlugin: CAPPlugin {
                 "requiredEntitlementIdentifier": requiredEntitlementIdentifier
             ]
 
-            if let offeringIdentifier = offeringIdentifier {
-                options["offeringIdentifier"] = offeringIdentifier
+            if let offeringOptions = offeringOptions {
+                options.merge(offeringOptions) { original, offeringOption in
+                    offeringOption
+                }
             }
 
             proxy.presentPaywallIfNeeded(
@@ -133,6 +137,46 @@ public class RevenueCatUIPlugin: CAPPlugin {
                     call.resolve()
                 }
             })
+        }
+    }
+}
+
+private extension RevenueCatUIPlugin {
+
+    func processOfferingOptions(_ call: CAPPluginCall) -> [String: Any]? {
+        let offering = call.getObject("offering")
+        let offeringIdentifier = call.getString("offeringIdentifier")
+        let availablePackages = offering?["availablePackages"] as? JSArray
+        let firstPackage = availablePackages?.first as? JSObject
+        let presentedOfferingContext = firstPackage?["presentedOfferingContext"] as? JSObject
+        let contextOfferingIdentifier = presentedOfferingContext?["offeringIdentifier"]
+        let contextPlacementIdentifier = presentedOfferingContext?["placementIdentifier"]
+        let contextTargetingContext = presentedOfferingContext?["targetingContext"] as? JSObject
+        let contextTargetingRevision = contextTargetingContext?["revision"]
+        let contextTargetingRuleId = contextTargetingContext?["ruleId"]
+        let displayCloseButton = call.getBool("displayCloseButton") ?? false
+
+        var options: [String: Any] = [:]
+        if let offeringIdentifier = offeringIdentifier {
+            options[PaywallProxy.PaywallOptionsKeys.offeringIdentifier] = offeringIdentifier
+            if let presentedOfferingContext = presentedOfferingContext,
+                let contextOfferingIdentifier = contextOfferingIdentifier {
+                var presentedOfferingContextMap = [
+                    PaywallProxy.PresentedOfferingContextKeys.offeringIdentifier: contextOfferingIdentifier,
+                    PaywallProxy.PresentedOfferingContextKeys.placementIdentifier: contextPlacementIdentifier,
+                ]
+                if let contextTargetingRevision = contextTargetingRevision,
+                    let contextTargetingRuleId {
+                    presentedOfferingContextMap[PaywallProxy.PresentedOfferingContextKeys.targetingContext] = [
+                        PaywallProxy.PresentedOfferingTargetingContextKeys.revision: contextTargetingRevision,
+                        PaywallProxy.PresentedOfferingTargetingContextKeys.ruleId: contextTargetingRuleId
+                    ]
+                }
+                options[PaywallProxy.PaywallOptionsKeys.presentedOfferingContext] = presentedOfferingContextMap
+            }
+            return options
+        } else {
+            return nil
         }
     }
 }

purchases-capacitor-ui/android/src/main/java/com/revenuecat/purchases/capacitor/ui/RevenueCatUIPlugin.kt
@@ -9,11 +9,13 @@ import com.getcapacitor.Plugin
 import com.getcapacitor.PluginCall
 import com.getcapacitor.PluginMethod
 import com.getcapacitor.annotation.CapacitorPlugin
+import com.revenuecat.purchases.PresentedOfferingContext
 import com.revenuecat.purchases.hybridcommon.ui.PaywallResultListener
 import com.revenuecat.purchases.hybridcommon.ui.PaywallSource
 import com.revenuecat.purchases.hybridcommon.ui.PresentPaywallOptions
 import com.revenuecat.purchases.hybridcommon.ui.presentPaywallFromFragment
 import com.revenuecat.purchases.ui.revenuecatui.customercenter.ShowCustomerCenter
+import org.json.JSONObject
 
 @CapacitorPlugin(name = "RevenueCatUI")
 class RevenueCatUIPlugin : Plugin(), PaywallResultListener {
@@ -38,11 +40,15 @@ class RevenueCatUIPlugin : Plugin(), PaywallResultListener {
     fun presentPaywall(call: PluginCall) {
         val offering = call.getObject("offering")
         val offeringIdentifier = offering?.getString("identifier")
+        val presentedOfferingContext = offering?.optJSONArray("availablePackages")
+            ?.optJSONObject(0)
+            ?.optJSONObject("presentedOfferingContext")
         val displayCloseButton = call.getBoolean("displayCloseButton") ?: false
 
         presentPaywallInternal(
             call = call,
             offeringIdentifier = offeringIdentifier,
+            presentedOfferingContext = presentedOfferingContext,
             displayCloseButton = displayCloseButton,
             requiredEntitlementIdentifier = null
         )
@@ -61,11 +67,15 @@ class RevenueCatUIPlugin : Plugin(), PaywallResultListener {
 
         val offering = call.getObject("offering")
         val offeringIdentifier = offering?.getString("identifier")
+        val presentedOfferingContext = offering?.optJSONArray("availablePackages")
+            ?.optJSONObject(0)
+            ?.optJSONObject("presentedOfferingContext")
         val displayCloseButton = call.getBoolean("displayCloseButton") ?: false
 
         presentPaywallInternal(
             call = call,
             offeringIdentifier = offeringIdentifier,
+            presentedOfferingContext = presentedOfferingContext,
             displayCloseButton = displayCloseButton,
             requiredEntitlementIdentifier = requiredEntitlementIdentifier
         )
@@ -77,6 +87,7 @@ class RevenueCatUIPlugin : Plugin(), PaywallResultListener {
     private fun presentPaywallInternal(
         call: PluginCall,
         offeringIdentifier: String?,
+        presentedOfferingContext: JSONObject?,
         displayCloseButton: Boolean,
         requiredEntitlementIdentifier: String?
     ) {
@@ -100,8 +111,34 @@ class RevenueCatUIPlugin : Plugin(), PaywallResultListener {
             return
         }
 
-        val paywallSource = offeringIdentifier?.let { PaywallSource.OfferingIdentifier(it) }
-            ?: PaywallSource.DefaultOffering
+        val presentedOfferingContext = presentedOfferingContext?.let { jsContext ->
+            val offeringId = jsContext.optString("offeringIdentifier").takeUnless { it.isNullOrEmpty() }
+            if (offeringId == null) { return@let null }
+            val placementIdentifier = jsContext.optString("placementIdentifier").takeUnless { it.isNullOrEmpty() }
+            val targetingContext = jsContext.optJSONObject("targetingContext")?.let { targetingContext ->
+                val revision = targetingContext.optInt("revision", -1).takeUnless { it == -1 }
+                val ruleId = targetingContext.optString("ruleId").takeUnless { it.isNullOrEmpty() }
+                if (revision == null || ruleId == null) { return@let null }
+                PresentedOfferingContext.TargetingContext(
+                    revision = revision,
+                    ruleId = ruleId,
+                )
+            }
+            PresentedOfferingContext(
+                offeringIdentifier = offeringId,
+                placementIdentifier = placementIdentifier,
+                targetingContext = targetingContext,
+            )
+        }
+
+        val paywallSource = if (offeringIdentifier != null && presentedOfferingContext != null) {
+            PaywallSource.OfferingIdentifierWithPresentedOfferingContext(
+                offeringIdentifier = offeringIdentifier,
+                presentedOfferingContext = presentedOfferingContext,
+            )
+        } else {
+            PaywallSource.DefaultOffering
+        }
 
         val options = PresentPaywallOptions(
             paywallSource = paywallSource,

commit 1830b493a8ee86692f76e3411dada8f4c01cabcd

Fix passing offering to `presentPaywall` functions (#572)
This broke in #524. We were incorrectly parsing the offering Identifier
in iOS native, which caused it to always show the default offering,
instead of the passed parameter. This was reported in #568.

purchases-capacitor-ui/ios/Plugin/Plugin/RevenueCatUIPlugin.swift
@@ -145,7 +145,7 @@ private extension RevenueCatUIPlugin {
 
     func processOfferingOptions(_ call: CAPPluginCall) -> [String: Any]? {
         let offering = call.getObject("offering")
-        let offeringIdentifier = call.getString("offeringIdentifier")
+        let offeringIdentifier = offering?["identifier"] as? String
         let availablePackages = offering?["availablePackages"] as? JSArray
         let firstPackage = availablePackages?.first as? JSObject
         let presentedOfferingContext = firstPackage?["presentedOfferingContext"] as? JSObject