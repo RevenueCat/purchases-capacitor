064d0d220e098155591bef8197f3bbc21c68341d
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2025-11-28 08:44:49 +0100

    Align package names with Cocoapods names

--- purchases-capacitor-ui/ios/Tests/RevenuecatPurchasesCapacitorUITests/PurchasesUIPluginTests.swift
+++ purchases-capacitor-ui/ios/Tests/RevenuecatPurchasesCapacitorUITests/PurchasesUIPluginTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import RevenuecatPurchasesCapacitor
+@testable import RevenuecatPurchasesCapacitorUi
 
-class PurchasesTests: XCTestCase {
+class PurchasesUIPluginTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -16,6 +16,6 @@ class PurchasesTests: XCTestCase {
         // This is an example of a functional test case for a plugin.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
 
-        let implementation = PurchasesPlugin()
+        let implementation = PurchasesUIPluginTests()
     }
 }
