24e698434a0fa415d4ddc09b4f749f5599bb1871
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2025-12-01 14:47:04 +0100

    Added Cocoapods installation test CI step

---
+++ ios/CocoapodsInstallationTest/CocoapodsInstallationTest/CocoapodsInstallationTestApp.swift
@@ -0,0 +1,20 @@
+//
+//  CocoapodsInstallationTestApp.swift
+//  CocoapodsInstallationTest
+//
+//  Created by Rick van der Linden on 01/12/2025.
+//
+
+import SwiftUI
+import Capacitor
+import RevenueCat
+import RevenueCatUI
+
+@main
+struct CocoapodsInstallationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
