e294b0fb242deb54f4e96e89de434a5261dcd26d
Rick <rick.vanderlinden@revenuecat.com>
2025-12-05 11:12:26 +0100

    Add SPM support (#608)
    Based on https://github.com/RevenueCat/purchases-capacitor/pull/595,
    adds Swift Package Manager support to the SDK.
    
    Migrated the example app to use SPM, since it's the default as of
    Capacitor 8. After migrating the main and UI library we no longer need
    their Xcode projects, since it's fully managed by the Package.swift
    file.
    
    In order to make sure we're not breaking Cocoapods support I've added
    the CocoapodsInstallationTest Xcode projects for both the main and the
    UI library. The CI will run `pod install` and ensures that the project
    is built successfully when ran.
    
    Todo
    - [x] Update release scripts
    - [x] Verify both libraries can be installed successfully in a new
    sample project through SPM
    - [x] Verify both libraries can be installed successfully in a new
    sample project through Cocoapods

---
+++ purchases-capacitor-ui/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "RevenuecatPurchasesCapacitorUi",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "RevenuecatPurchasesCapacitorUi",
+            targets: ["RevenuecatPurchasesCapacitorUi"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+         .package(url: "https://github.com/RevenueCat/purchases-hybrid-common.git", exact: "17.21.0")
+    ],
+    targets: [
+        .target(
+            name: "RevenuecatPurchasesCapacitorUi",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "PurchasesHybridCommonUI", package: "purchases-hybrid-common")
+            ],
+            path: "ios/Sources/RevenuecatPurchasesCapacitorUI")
+    ]
+)
---
+++ purchases-capacitor-ui/ios/CocoapodsInstallationTest/CocoapodsInstallationTest/CocoapodsInstallationTestApp.swift
@@ -0,0 +1,19 @@
+//
+//  CocoapodsInstallationTestApp.swift
+//  CocoapodsInstallationTest
+//
+//  Created by Rick van der Linden on 01/12/2025.
+//
+
+import SwiftUI
+import Capacitor
+import RevenueCatUI
+
+@main
+struct CocoapodsInstallationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
