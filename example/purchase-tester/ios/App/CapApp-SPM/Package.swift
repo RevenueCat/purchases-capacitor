0075b65acfa176ff00b746fa2906ad40d62e0400
Oleksandr Dudynets <dudynetss@gmail.com>
2025-12-26 16:34:00 +0200

    [EXTERNAL] chore: add support for Capacitor 8 (#632)
    Adds support for Capacitor 8.
    
    - Bumps iOS minimum version to 15.0 and updates Capacitor Swift PM to
    v8.
    - Updates Android SDK levels and AndroidX deps.
    - Upgrades Capacitor packages, peer deps, and dev tooling to
    v8-compatible versions.
    
    Fixes #627

--- example/purchase-tester/ios/App/CapApp-SPM/Package.swift
+++ example/purchase-tester/ios/App/CapApp-SPM/Package.swift
@@ -4,21 +4,21 @@ import PackageDescription
 // DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
 let package = Package(
     name: "CapApp-SPM",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapApp-SPM",
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "7.4.4"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.0"),
         .package(name: "CapacitorApp", path: "../../../node_modules/@capacitor/app"),
+        .package(name: "CapacitorDialog", path: "../../../node_modules/@capacitor/dialog"),
         .package(name: "CapacitorHaptics", path: "../../../node_modules/@capacitor/haptics"),
         .package(name: "CapacitorKeyboard", path: "../../../node_modules/@capacitor/keyboard"),
         .package(name: "CapacitorStatusBar", path: "../../../node_modules/@capacitor/status-bar"),
-        .package(name: "CapacitorDialog", path: "../../../node_modules/@capacitor/dialog"),
-        .package(name: "RevenuecatPurchasesCapacitor", path: "../../../../.."),
-        .package(name: "RevenuecatPurchasesCapacitorUi", path: "../../../../../purchases-capacitor-ui")
+        .package(name: "RevenuecatPurchasesCapacitor", path: "../../../node_modules/@revenuecat/purchases-capacitor"),
+        .package(name: "RevenuecatPurchasesCapacitorUi", path: "../../../node_modules/@revenuecat/purchases-capacitor-ui")
     ],
     targets: [
         .target(
@@ -27,10 +27,10 @@ let package = Package(
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "CapacitorApp", package: "CapacitorApp"),
+                .product(name: "CapacitorDialog", package: "CapacitorDialog"),
                 .product(name: "CapacitorHaptics", package: "CapacitorHaptics"),
                 .product(name: "CapacitorKeyboard", package: "CapacitorKeyboard"),
                 .product(name: "CapacitorStatusBar", package: "CapacitorStatusBar"),
-                .product(name: "CapacitorDialog", package: "CapacitorDialog"),
                 .product(name: "RevenuecatPurchasesCapacitor", package: "RevenuecatPurchasesCapacitor"),
                 .product(name: "RevenuecatPurchasesCapacitorUi", package: "RevenuecatPurchasesCapacitorUi")
             ]
