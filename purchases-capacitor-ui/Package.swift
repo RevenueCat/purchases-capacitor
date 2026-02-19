c72acbadbb7d5575affbd8a22b189deefa92a5ae
RCGitBot <dev+RCGitBot@revenuecat.com>
2026-02-19 19:06:34 +0000

    Version bump for 17.41.0

--- purchases-capacitor-ui/Package.swift
+++ purchases-capacitor-ui/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
-         .package(url: "https://github.com/RevenueCat/purchases-hybrid-common.git", exact: "17.40.0")
+         .package(url: "https://github.com/RevenueCat/purchases-hybrid-common.git", exact: "17.41.0")
     ],
     targets: [
         .target(
