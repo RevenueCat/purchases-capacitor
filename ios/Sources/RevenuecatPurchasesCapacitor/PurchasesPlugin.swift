13a0d0c583f81ae63485c9b678866334aeefda49
RCGitBot <dev+RCGitBot@revenuecat.com>
2026-02-23 15:22:51 +0000

    Version bump for 12.2.0

--- ios/Sources/RevenuecatPurchasesCapacitor/PurchasesPlugin.swift
+++ ios/Sources/RevenuecatPurchasesCapacitor/PurchasesPlugin.swift
@@ -89,7 +89,7 @@ public class PurchasesPlugin: CAPPlugin, PurchasesDelegate, CAPBridgedPlugin {
         CAPPluginMethod(name: "overridePreferredUILocale", returnType: CAPPluginReturnNone),
     ] 
     private let platformFlavor = "capacitor"
-    private let platformVersion = "12.1.3"
+    private let platformVersion = "12.2.0"
 
     private let customerInfoKey = "customerInfo"
     private let transactionKey = "transaction"
