7304e65fffced44019ea649d6d50c69da1ea179f
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2026-03-12 15:14:28 +0100

    Address additional review feedback for API tests
    - Add missing enums: VERIFICATION_RESULT, PRODUCT_TYPE, PURCHASE_TYPE,
      PAYWALL_RESULT, WebPurchaseRedemptionResultType
    - Add missing type tests: PurchasesSubscriptionInfo, PurchasesVirtualCurrency,
      PurchaseParams, ShouldPurchasePromoProductListener, MakePurchaseResult
    - Add missing type aliases: Store, OwnershipType, PeriodType
    - Add missing properties on PurchasesEntitlementInfo (8 fields),
      PurchasesEntitlementInfos (verification), CustomerInfo
      (subscriptionsByProductIdentifier), PurchasesStoreProduct (productType),
      PurchasesPackage (webCheckoutUrl), PurchasesOffering (webCheckoutUrl)
    - Add missing PurchasesConfiguration options (6 fields)
    - Add missing TEST_STORE_SIMULATED_PURCHASE_ERROR error code

--- apitesters/customerInfo.ts
+++ apitesters/customerInfo.ts
@@ -1,9 +1,14 @@
 import type {
   CustomerInfo,
   LogInResult,
+  OwnershipType,
+  PeriodType,
   PurchasesEntitlementInfo,
   PurchasesEntitlementInfos,
   PurchasesStoreTransaction,
+  PurchasesSubscriptionInfo,
+  Store,
+  VERIFICATION_RESULT,
 } from '../src/definitions';
 
 function checkLoginResult(result: LogInResult) {
@@ -25,11 +30,14 @@ function checkCustomerInfo(info: CustomerInfo) {
   const originalPurchaseDate: string | null = info.originalPurchaseDate;
   const managementURL: string | null = info.managementURL;
   const nonSubscriptionTransactions: PurchasesStoreTransaction[] = info.nonSubscriptionTransactions;
+  const subscriptionsByProductIdentifier: { [key: string]: PurchasesSubscriptionInfo } =
+    info.subscriptionsByProductIdentifier;
 }
 
 function checkEntitlementInfos(infos: PurchasesEntitlementInfos) {
   const all: { [p: string]: PurchasesEntitlementInfo } = infos.all;
   const active: { [p: string]: PurchasesEntitlementInfo } = infos.active;
+  const verification: VERIFICATION_RESULT = infos.verification;
 }
 
 function checkEntitlementInfo(info: PurchasesEntitlementInfo) {
@@ -45,6 +53,32 @@ function checkEntitlementInfo(info: PurchasesEntitlementInfo) {
   const isSandbox: boolean = info.isSandbox;
   const unsubscribeDetectedAt: string | null = info.unsubscribeDetectedAt;
   const billingIssueDetectedAt: string | null = info.billingIssueDetectedAt;
+  const latestPurchaseDateMillis: number = info.latestPurchaseDateMillis;
+  const originalPurchaseDateMillis: number = info.originalPurchaseDateMillis;
+  const expirationDateMillis: number | null = info.expirationDateMillis;
+  const unsubscribeDetectedAtMillis: number | null = info.unsubscribeDetectedAtMillis;
+  const billingIssueDetectedAtMillis: number | null = info.billingIssueDetectedAtMillis;
+  const ownershipType: OwnershipType = info.ownershipType;
+  const verification: VERIFICATION_RESULT = info.verification;
+  const productPlanIdentifier: string | null = info.productPlanIdentifier;
+}
+
+function checkSubscriptionInfo(info: PurchasesSubscriptionInfo) {
+  const productIdentifier: string = info.productIdentifier;
+  const purchaseDate: string = info.purchaseDate;
+  const originalPurchaseDate: string | null = info.originalPurchaseDate;
+  const expiresDate: string | null = info.expiresDate;
+  const store: Store = info.store;
+  const unsubscribeDetectedAt: string | null = info.unsubscribeDetectedAt;
+  const isSandbox: boolean = info.isSandbox;
+  const billingIssuesDetectedAt: string | null = info.billingIssuesDetectedAt;
+  const gracePeriodExpiresDate: string | null = info.gracePeriodExpiresDate;
+  const ownershipType: OwnershipType = info.ownershipType;
+  const periodType: PeriodType = info.periodType;
+  const refundedAt: string | null = info.refundedAt;
+  const storeTransactionId: string | null = info.storeTransactionId;
+  const isActive: boolean = info.isActive;
+  const willRenew: boolean = info.willRenew;
 }
 
 function checkTransaction(transaction: PurchasesStoreTransaction) {
