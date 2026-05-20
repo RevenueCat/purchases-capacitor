02eacee0a03512371a1a88ae02615171e8631db9
RCGitBot <dev+RCGitBot@revenuecat.com>
2026-05-20 17:07:31 +0000

    Version bump for 13.1.2

--- ios/Sources/RevenuecatPurchasesCapacitor/PurchasesPlugin.swift
+++ ios/Sources/RevenuecatPurchasesCapacitor/PurchasesPlugin.swift
@@ -90,7 +90,7 @@ public class PurchasesPlugin: CAPPlugin, PurchasesDelegate, CAPBridgedPlugin {
         CAPPluginMethod(name: "trackCustomPaywallImpression", returnType: CAPPluginReturnNone),
     ]
     private let platformFlavor = "capacitor"
-    private let platformVersion = "13.1.1"
+    private let platformVersion = "13.1.2"
 
     private let customerInfoKey = "customerInfo"
     private let transactionKey = "transaction"
